a806f091c044241fc6f40c85fff8aa61936d3a01
Kevin <kevinlee.3191@gmail.com>
2023-11-29 12:40:58 -0800

    basic login/register

--- App.js
+++ App.js
@@ -1,33 +1,97 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
-import * as firebase from 'firebase/app';
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID,
+//   measurementId: process.env.MEASUREMENT_ID
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnW7Oq4f705jxGvYMBbsFchqEYFPcT2DE",
+  authDomain: "instagram-clone-d2fd1.firebaseapp.com",
+  projectId: "instagram-clone-d2fd1",
+  storageBucket: "instagram-clone-d2fd1.appspot.com",
+  messagingSenderId: "875429250613",
+  appId: "1:875429250613:web:ea5046f2caecb0d285537c",
+  measurementId: "G-E5VLR51LN9"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Landing from './components/auth/Landing';
+import Register from './components/auth/Register';
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
-};
+
 
 const Stack = createStackNavigator();
-console.log(process.env.API_KEY)
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" component={Landing} options={{ headerShown: false }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if(!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+      }
+      else
+      {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>{"Loading"}</Text>
+        </View>
+      )
+    }
+    if(!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={Landing} options={{ headerShown: false }}/>
+            <Stack.Screen name="Register" component={Register}/>
+  
+          </Stack.Navigator>
+        </NavigationContainer>
+      )
+    }
+    return (
+      <View  style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>{"User is logged in"}</Text>
+      </View>
+    )
+  }
 }
 
+export default App;
--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import { View, Button, TextInput } from 'react-native';
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-export class Register extends Component {
+export class Login extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             email: '',
-            password: '',
-            name: ''
+            password: ''
         };
 
-        this.onSignUp = this.onSignUp.bind(this);
+        this.onSignIn = this.onSignIn.bind(this);
     }
 
-    onSignUp() {
+    onSignIn() {
         const auth = getAuth();
-        const { email, password, name } = this.state;
-        createUserWithEmailAndPassword(auth, email, password).then((result) => {
+        const { email, password } = this.state;
+        signInWithEmailAndPassword(auth, email, password).then((result) => {
             console.log(result);
         }).catch((result) => {
             console.log(result);
@@ -29,10 +28,6 @@ export class Register extends Component {
     render() {
         return (
             <View>
-                <TextInput 
-                    placeholder="name"
-                    onChangeText={(name) => this.setState({ name })}
-                />
                 <TextInput 
                     placeholder="email"
                     onChangeText={(email) => this.setState({ email })}
@@ -44,12 +39,12 @@ export class Register extends Component {
                 />
 
                 <Button 
-                    onPress={() => this.onSignUp()}
-                    title="Sign Up"
+                    onPress={() => this.onSignIn()}
+                    title="Sign In"
                 />
             </View>
         )
     }
 }
 
-export default Register;
+export default Login;
