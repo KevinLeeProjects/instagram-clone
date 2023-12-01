1bc7fe4a7c7e087bfebdb03f565bf61996484ea2
Kevin <kevinlee.3191@gmail.com>
2023-12-01 01:53:03 -0800

    style changes

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -8,8 +8,35 @@ function Profile(props) {
   const { currentUser, posts } = props;
   console.log(`Here ${JSON.stringify({currentUser, posts})}`);
   return (
-    <SafeAreaView className="w-[100vw] h-[100dvh] items-center text-center flex-1 flex flex-col">
-        <Text>{currentUser.name}</Text>
+    <SafeAreaView className="w-[100vw] h-[100dvh] flex-1 flex flex-col">
+        <Text className="text-2xl font-bold">{currentUser.name}</Text>
+        <View className="flex flex-col bg-green-500">
+          {/* PFP and followers count */}
+          <View className="flex flex-row">
+            <Text className="w-[25vw]">{"PFP"}</Text>
+            <View className="flex-1 flex-row justify-between">
+              <View className="flex flex-col items-center">
+                <Text>{"0"}</Text>
+                <Text>{"Post Count"}</Text>
+              </View>
+              <View className="flex flex-col items-center">
+                <Text>{"0"}</Text>
+                <Text>{"Followers Count"}</Text>
+              </View>
+              <View className="flex flex-col items-center">
+                <Text>{"0"}</Text>
+                <Text>{"Following Count"}</Text>
+              </View>
+            </View>
+          </View>
+
+          {/* Name and Bio */}
+          <View className="flex flex-col mt-[10px]">
+            <Text>{"Name"}</Text>
+            <Text>{"Bio"}</Text>
+            <Text>{"Website"}</Text>
+          </View>
+        </View>
         <View>
           <FlatList 
             className="w-[100vw]"
